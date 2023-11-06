c753052a36fc0f932b42a32ac0460853f1fb0a64
rv452njit <141543852+rv452njit@users.noreply.github.com>
2023-11-06 12:31:40 -0500

    closes #49

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 {import('next').NextConfig}
-
+const pathPrefix = process.env.NODE_ENV === 'production'
+  ? 'https://njit-wis.github.io/midterm-portfolio-rv452njit/'
+  : '';
 const nextConfig = {
     output: 'export',
     exportTrailingSlash: true,
     basePath: '/github-pages',
+    assetPrefix: 'https://njit-wis.github.io/midterm-portfolio-rv452njit/',
+      env: {
+        pathPrefix,
+      },
 }
 
 module.exports = {nextConfig}
